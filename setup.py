bdcca042c667987433caa8861f57d7570316ecfb
pogi <pogi@obsd68.my.domain>
2021-01-15 20:29:39 +0800

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="py-bsdauth",
-    version="1.0.0",
+    version="1.1.0",
     description="Python interface to OpenBSD's BSD Auth api in libc.",
     long_description=README,
     long_description_content_type="text/markdown",
